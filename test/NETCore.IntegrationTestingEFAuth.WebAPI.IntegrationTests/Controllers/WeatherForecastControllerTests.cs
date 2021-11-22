d9bbc54f22a714e87fac61b22f7cd18b91c6e9d1
Ankan <ankan.mookherjee@newday.co.uk>
2021-11-22 18:50:22 +0000

    Fake policy evaluator to bypass authorize attribute for testing in asp.net core 3.1

---
+++ test/NETCore.IntegrationTestingEFAuth.WebAPI.IntegrationTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace NETCore.IntegrationTestingEFAuth.WebAPI.IntegrationTests.Controllers
+{
+    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public WeatherForecastControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                        {
+                            services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                        });
+            }).CreateClient();
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var response = (await _client.GetAsync("/weatherforecast")).EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<WeatherForecast[]>(stringResponse);
+
+            Assert.Equal(5, result.Length);
+        }
+    }
+}
